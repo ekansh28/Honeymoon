29d41beebc162f7acaf1be29c26bd161e95bdf63
Ekansh Sharma <70947204+ekansh28@users.noreply.github.com>
2024-11-04 17:52:37 +0530

    Initial commit

---
+++ HONEYMOONApp.swift
@@ -0,0 +1,17 @@
+//
+//  HONEYMOONApp.swift
+//  HONEYMOON
+//
+//  Created by Ekansh  on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HONEYMOONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
